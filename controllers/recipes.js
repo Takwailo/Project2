4bd2ddbcb627eec0b08a22b2f8bfec33432305f7
takwai <takwai0318@gmail.com>
2022-04-11 13:36:59 -0400

    add cuisine

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -1,15 +1,19 @@
-const request = require('request');
+const request = require("request");
+const Ingredients = require("../models/ingredient");
+const Recipes = require("../models/recipe");
 
+function index(req, res) {
+  res.render("recipes/index", { title: "All Recipes" });
+}
 
-function index(req, res){
-        res.render('recipes/index', {title: "All Recipes"})
-    }
-
-function addRecipe(req, res){
-    res.render('recipes/new', {title: 'Add New Recipes'})
+function addRecipe(req, res) {
+  Ingredients.find({}, function (err, ingredient) {
+      console.log(ingredient)
+    res.render("recipes/new", {ingredient, title: "Add New Recipes" });
+  });
 }
 
 module.exports = {
-    index,
-    new: addRecipe,
-  };
+  index,
+  new: addRecipe,
+};
--- models/recipe.js
+++ models/recipe.js
@@ -1,27 +1,22 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const cuisineSchema = new Schema({
-    type: String,
-    enum: ['Italian', 'Japanese', 'Chinese', 'Indian', 'American'],
-    required: true
+  type: String,
+  enum: ["Italian", "Japanese", "Chinese", "Indian", "American"],
 });
-
 
 const recipesSchema = new Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    ingredients: [{type: Schema.Types.ObjectId, ref: 'Ingredients'}],
-    method: {
-        type: String,
-        require: true
-    },
-    crusine: [cuisineSchema],
-    user: {type: Schema.Types.ObjectId, ref: 'User'},
-    timestamps: true,
-    like: Boolean,
+  title: {
+    type: String,
+  },
+  ingredients: [{ type: Schema.Types.ObjectId, ref: "Ingredients" }],
+  method: {
+    type: String,
+  },
+  crusine: [cuisineSchema],
+  user: { type: Schema.Types.ObjectId, ref: "User" },
+  like: Boolean,
 });
 
-module.exports = mongoose.model('Recipes', recipesSchema);
+module.exports = mongoose.model("Recipes", recipesSchema);
